15b703d0595c8474e18ea1e7f8a14ca68a3d2749
Amardeep singh <Amardeep@Macs-MacBook-Pro-767.local>
2026-04-07 17:44:29 +0530

    Add new files

---
+++ Max.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Max_M {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);   // add element
+        list.add(20);
+        list.remove(1);
+
+        System.out.println(list);  // [10, 20, 30]
+    }
+}
